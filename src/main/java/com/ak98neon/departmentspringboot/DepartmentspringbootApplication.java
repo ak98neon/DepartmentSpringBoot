bd6ca20dc6751feb453b7168acec51f2ad8d969f
akudria <ak98nneon@gmail.com>
2018-11-23 10:40:24 +0200

    Initial Projects

---
+++ src/main/java/com/ak98neon/departmentspringboot/DepartmentspringbootApplication.java
@@ -0,0 +1,12 @@
+package com.ak98neon.departmentspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DepartmentspringbootApplication.class, args);
+    }
+}
