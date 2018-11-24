edc795f5ad3950b32996e09ca02909ce1ffb7657
artemkudrya <ak98nneon@gmail.com>
2018-11-25 01:16:48 +0200

    edit redirect variable, make her static final, remove @Value annotation, and remove {id} parameter for edit department.

--- src/main/java/com/ak98neon/departmentspringboot/controller/department/DepartmentController.java
+++ src/main/java/com/ak98neon/departmentspringboot/controller/department/DepartmentController.java
@@ -3,7 +3,6 @@ package com.ak98neon.departmentspringboot.controller.department;
 import com.ak98neon.departmentspringboot.entity.Department;
 import com.ak98neon.departmentspringboot.service.department.DepartmentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,10 +15,7 @@ import java.util.List;
 @Controller
 public class DepartmentController {
     private DepartmentServiceImpl departmentService;
-
-    @Value("redirect:/listDepartment")
-    private String REDIRECT;
-
+    private static final String REDIRECT_LIST_DEPARTMENT = "redirect:/listDepartment";
 
     @Autowired
     public void setDepartmentService(DepartmentServiceImpl departmentService) {
@@ -41,7 +37,7 @@ public class DepartmentController {
     @PostMapping("/addDepartment")
     public String saveDepartment(@RequestParam(value = "name") String name) {
         departmentService.saveDepartment(new Department(name));
-        return REDIRECT;
+        return REDIRECT_LIST_DEPARTMENT;
     }
 
     @GetMapping("/editDepartment/{id}")
@@ -50,15 +46,15 @@ public class DepartmentController {
         return "/department/editDepartment";
     }
 
-    @PostMapping("/editDepartment/{id}")
-    public String saveEditDepartment(@PathVariable("id") Long id, @RequestParam(value = "name") String name) {
+    @PostMapping("/editDepartment")
+    public String saveEditDepartment(@RequestParam("id") Long id, @RequestParam(value = "name") String name) {
         departmentService.editDepartmentById(id, name);
-        return REDIRECT;
+        return REDIRECT_LIST_DEPARTMENT;
     }
 
     @GetMapping("/deleteDepartment/{id}")
     public String deleteDepartment(@PathVariable("id") Long id) {
         departmentService.deleteDepartmentById(id);
-        return REDIRECT;
+        return REDIRECT_LIST_DEPARTMENT;
     }
 }
